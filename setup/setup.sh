849733d7f4ed017890c4592e70c692037da9e691
mmorii <morii@ism.ac.jp>
2017-10-17 12:09:59 +0900

    modify

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,7 +16,7 @@
 ##
 
 export MITOOL=/home/morii/work/github/moriiism/mitool
-export LD_LIBRARY_PATH=/soft/minuit2/5.34.14/lib:/soft/root/6.08.02/lib:/soft/gsl/2.3/lib:/usr/lib/gcc/x86_64-redhat-linux/4.4.4:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=/soft/root/6.08.02/lib:/soft/gsl/2.3/lib:${LD_LIBRARY_PATH}
 export PATH=/soft/root/6.08.02/bin:${PATH}
 
 alias root="root -l"
@@ -26,19 +26,10 @@ alias root="root -l"
 #
 
 HEADAS_VER=6.16
-# export HEADAS=/soft/heasoft/heasoft-${HEADAS_VER}/x86_64-unknown-linux-gnu-libc2.12
-export HEADAS=/soft/heasoft/${HEADAS_VER}/x86_64-unknown-linux-gnu-libc2.17
+export HEADAS=/soft/heasoft/heasoft-${HEADAS_VER}/x86_64-unknown-linux-gnu-libc2.12
+# export HEADAS=/soft/heasoft/${HEADAS_VER}/x86_64-unknown-linux-gnu-libc2.17
 source $HEADAS/headas-init.sh
 export PGPLOT_TYPE=/xw
-
-
-<<<<<<< HEAD
-=======
-# debug
-# export LD_LIBRARY_PATH=/soft/minuit2/5.34.14/lib:${LD_LIBRARY_PATH}
-
-
->>>>>>> 48c9309aed30e511adb1e474da0723fed144be8c
 
 ################################################################
 ################################################################
